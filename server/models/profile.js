b307b86237de47fc54956598195ff72bd1e138bc
Amita <panwar.amita@gmail.com>
2022-01-18 21:34:41 -0500

    added a profile model

---
+++ server/models/profile.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const avaliableTime = new mongoose.Schema({
+    avaliableDay: {
+        type: Date,
+        required: true,
+    },
+    timeTo: {
+        type: String,
+        required: true,
+    },
+    timeFrom: {
+        type: String,
+        required: true
+    },
+})
+const profileSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: false,
+    },
+    lastName: {
+        type: String,
+        required: false,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    phoneNo: {
+        type: Number,
+        required: false,
+    },
+    photoUrl: {
+        type: String,
+        required: false,
+    },
+    email: {
+        type: String,
+        required: false
+    },
+    city: {
+        type: String,
+        required: false
+    },
+    DateOfBirth: {
+        type: String,
+        required: false
+    },
+    avaliabilty: [avaliableTime],
+});
+module.exports = Profile = mongoose.model("Profile", profileSchema);
