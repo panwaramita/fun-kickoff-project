5bcc885b7a00ebec45b4aada75f325d551d38d74
Amita <panwar.amita@gmail.com>
2022-01-20 20:43:25 -0500

    update changes for profile page

--- server/test/ping.spec.js
+++ server/test/ping.spec.js
@@ -1,18 +1,21 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const { server } = require("../app.js");
+const app = require("../app.js");
 
 chai.should();
 chai.use(chaiHttp);
 
-describe("/POST /auth/logout", () => {
+describe("/POST ping", () => {
   it("it should return 200 and message", (done) => {
     chai
-      .request(server)
-      .post('/auth/logout/')
+      .request(app)
+      .post(`/ping/`)
+      .send({ message: "hello" })
       .end((err, res) => {
         res.should.have.status(200);
-        res.text.should.equal("You have successfully logged out");
+        res.body.should.have
+          .property("response")
+          .eql("Server is running. Message received: hello");
         done();
       });
   });
